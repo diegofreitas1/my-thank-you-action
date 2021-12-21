98f9cc80761868029c72fac774ebf5d80e496bfc
diegofreitas1 <diegofreitas13@gmail.com>
2021-12-21 17:37:15 -0300

    saying thank you to new pull requests

--- dist/index.js
+++ dist/index.js
@@ -15254,26 +15254,17 @@ var __webpack_exports__ = {};
 const fetch = __nccwpck_require__(7580);
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
+const { context } = __nccwpck_require__(5438)
+const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+const octokit = github.getOctokit(GITHUB_TOKEN);
+
+const { pull_request } = context.payload;
 
 async function run() {
-
-  const { context } = __nccwpck_require__(5438)
-  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-  const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
-  const octokit = github.getOctokit(GITHUB_TOKEN);
-  
-  const { pull_request } = context.payload;
-  
-  const randomPos = Math.round(Math.random() * 1000);
-  const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
-  const response = await fetch(url);
-  const { results } = await response.json();
-  const gifUrl = results[0].media[0].tinygif.url;   
-
 await octokit.rest.issues.createComment({
 	...context.repo,
 	issue_number: pull_request.number,
-	body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.\n\n<img src="${gifUrl}" alt="thank you" />'
+	body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
 });
 }
 
