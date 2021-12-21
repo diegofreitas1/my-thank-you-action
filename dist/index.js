83b6d1f338dab91300e0e9dd8f5099614025691a
diegofreitas1 <diegofreitas13@gmail.com>
2021-12-21 17:09:39 -0300

    saying thank you to new pull requests

--- dist/index.js
+++ dist/index.js
@@ -8444,21 +8444,19 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
-
+const { context } = __nccwpck_require__(5438)
+const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+const octokit = github.getOctokit(GITHUB_TOKEN);
+  
+const { pull_request } = context.payload;
+  
 async function run() {
-  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-
-  const octokit = github.getOctokit(GITHUB_TOKEN);
-
-  const { context = {} } = github;
-  const { pull_request } = context.payload;
-
-  await octokit.issues.createComment({
+  await octokit.rest.issues.createComment({
     ...context.repo,
     issue_number: pull_request.number,
     body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
   });
-  }
+}
   
 run();
 })();
