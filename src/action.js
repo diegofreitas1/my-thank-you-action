645c01857f968942dd385c668f0fe8e74b43d4c5
diegofreitas1 <diegofreitas13@gmail.com>
2021-12-22 17:14:20 -0300

    cr7 random

--- src/action.js
+++ src/action.js
@@ -8,7 +8,7 @@ async function run() {
   const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
   
   const randomPos = Math.round(Math.random() * 1000);
-  const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
+  const url = `https://api.tenor.com/v1/search?q=cristiano%20ronaldo&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
   
   const response = await fetch(url);
   const { results } = await response.json();
